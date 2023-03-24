7fc81bed85cfb5279d3be13d420dcf8d65358a2b
Brandon <bmckinzie@apple.com>
2023-03-23 20:41:27 -0700

    Upload notes

---
+++ DeepLearningNotes/combine_all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.6
+
+file_names = [
+
+MiscStanford.tex*
+tikzlibrarybayesnet.code.tex*
+CondensedSummaries.tex*
+BlogsAndAppendix.tex*
+Textbooks.tex*
+DeepLearningBook.tex*
+PapersAndTutorials.tex*
+
+]
+
+
